41694e36b3f85e7010ca1d46e6b1eda90072bb43
PublicCo <410973142@qq.com>
2023-08-06 21:49:51 +0800

    lab 9

--- kernel/fs.h
+++ kernel/fs.h
@@ -24,9 +24,11 @@ struct superblock {
 
 #define FSMAGIC 0x10203040
 
-#define NDIRECT 12
+#define NDIRECT 11
 #define NINDIRECT (BSIZE / sizeof(uint))
-#define MAXFILE (NDIRECT + NINDIRECT)
+//二级索引，256*256（block1024，每个索引都是uint）
+#define SECONDDIRECT (BSIZE / sizeof(uint))*(BSIZE / sizeof(uint))
+#define MAXFILE (NDIRECT + NINDIRECT+SECONDDIRECT)
 
 // On-disk inode structure
 struct dinode {
@@ -35,7 +37,7 @@ struct dinode {
   short minor;          // Minor device number (T_DEVICE only)
   short nlink;          // Number of links to inode in file system
   uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  uint addrs[NDIRECT+2];   // Data block addresses
 };
 
 // Inodes per block.
--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -316,6 +316,30 @@ sys_open(void)
     }
   }
 
+  int searchtime=0;
+  while(ip->type==T_SYMLINK&&!(omode&O_NOFOLLOW)){
+    searchtime++;
+    //防止链接成环，设置上限为10
+    if(searchtime>=10){
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+    // 读取对应的 inode存储的target路径
+    if(readi(ip, 0, (uint64)path, 0, MAXPATH) < MAXPATH) {
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+    iunlockput(ip);
+    // 根据文件名称找到链接上的的 inode
+    if((ip = namei(path)) == 0) {
+      end_op();
+      return -1;
+    }
+    ilock(ip);
+  }
+
   if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
     iunlockput(ip);
     end_op();
@@ -483,4 +507,31 @@ sys_pipe(void)
     return -1;
   }
   return 0;
+}
+
+uint64 sys_symlink(void){
+  char path[MAXPATH],target[MAXPATH];
+  struct inode* ip;
+  if(argstr(0,target,MAXPATH)<0)
+    return -1;
+  if(argstr(1,path,MAXPATH)<0)
+    return -1;
+  //开始事务
+  begin_op();
+   // 为这个符号链接新建一个 inode
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0) {
+    end_op();
+    return -1;
+  }
+  // 在符号链接的 data 中写入被链接的文件
+  if(writei(ip, 0, (uint64)target, 0, MAXPATH) < MAXPATH) {
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  //commit
+   iunlockput(ip);
+   end_op();
+   return 0;
 }
