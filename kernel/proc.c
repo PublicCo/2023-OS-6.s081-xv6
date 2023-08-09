b69c4aeafc8ec6a2f130b456708140d751776ba4
PublicCo <410973142@qq.com>
2023-08-09 16:56:14 +0800

    lab3 added

--- kernel/proc.c
+++ kernel/proc.c
@@ -169,11 +169,12 @@ freeproc(struct proc *p)
 {
   if (p->trapframe)
     kfree((void *)p->trapframe);
+  if (p->usyscall)
+    kfree((void *)p->usyscall);
   p->trapframe = 0;
   if (p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
-  if (p->usyscall)
-    kfree((void *)p->usyscall);
+
   p->usyscall = 0;
   p->pagetable = 0;
   p->sz = 0;
